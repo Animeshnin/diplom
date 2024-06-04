4e7ce3e77852e2dc78d8888c044dfe78e74c0391
Ildar_Zabirof <uldar82@gmail.com>
2024-06-04 17:31:42 +0500

    Initial commit

---
+++ diplom/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';  // Импорт стилей для компонента App
+import SpeedTypingTest from "./components/SpeedTypingTest"; // Импорт компонента SpeedTypingTest
+
+
+function App() {
+  return (
+    <div className="App">
+      <SpeedTypingTest/>
+    </div>
+  );
+}
+
+export default App;
